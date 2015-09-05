311dacb10dd91fdc34def1c6ce6f7bc08f9b9c7a
Bonnie <aalle.allen@gmail.com>
2015-09-05 12:32:53 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var Header = React.createClass({
+    render: function () {
+        return (
+            <h1 className="title">InoDirect</h1>
+        );
+    }
+});
+
+var SearchBar = React.createClass({
+    render: function () {
+        return (
+            <input type="search" />
+        );
+    }
+});
+ 
+var StaffList = React.createClass({
+    render: function () {
+        return (
+            <ul>
+                <li>Apple Moses</li>
+                <li>Wally Beaver</li>
+                <li>Ward Cleaver</li>
+                <li>Betty Hampton</li>
+            </ul>
+        );
+    }
+});
+
+var HomePage = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Header text="Staff Directory"/>
+                <SearchBar />
+                <StaffList />
+            </div>
+        );
+    }
+});
+
+React.render(
+    <HomePage />,
+    document.body
+);
