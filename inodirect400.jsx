f90f04a252e9cbbd5249a7cb24662c5a7b63e096
Bonnie <aalle.allen@gmail.com>
2015-09-08 09:13:59 -0400

    trying to render search bar

---
+++ inodirect400.jsx
@@ -0,0 +1,14 @@
+/**
+ *
+ * This directive is necessary to enable preprocessing of JSX tags:
+ * @jsx React.DOM
+ */
+
+var SearchBar = React.createClass({
+    render: function () {
+        return (
+            <input type="search" />
+        );
+    }
+});
+
